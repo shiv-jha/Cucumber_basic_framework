2863a0889376739911e1de6cef2dd3454e78569e
shiv.jha <shiv.jha@servicenow.com>
2022-08-01 07:51:57 +0530

    test commit

--- src/code/Math.java
+++ src/code/Math.java
@@ -4,7 +4,7 @@ package code;
 public class Math {	
 
 	public Math(){}
-
+	//Starting function	
 	public int Substract(int firstNumber, int secondNumber)
 	{
 		return (firstNumber - secondNumber);		
